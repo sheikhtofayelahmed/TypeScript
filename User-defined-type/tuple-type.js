c31342d9c52b070a74389e45d9b9f050799a202c
tofayel101 <tofayel101@proton.me>
2022-11-20 16:34:17 +0600

    tuple

---
+++ User-defined-type/tuple-type.js
@@ -0,0 +1,4 @@
+var tupleType;
+tupleType = ["101", 101];
+tupleType.push("jkdjf");
+console.log(tupleType);
---
+++ User-defined-type/tuple-type.ts
@@ -0,0 +1,4 @@
+let tupleType: [string, number];
+tupleType=["101",101]
+tupleType.push("jkdjf")
+console.log(tupleType);
