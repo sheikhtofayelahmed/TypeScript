d5de792372a7b3d27c44a6dc1f9057540f6990ba
tofayel101 <tofayel101@proton.me>
2022-11-20 15:48:04 +0600

    built in type

---
+++ Built-in-type/index.js
@@ -0,0 +1,10 @@
+var userId;
+var userName;
+var isActive;
+userId = 101;
+userName = "TOFAYEL";
+isActive = true;
+console.log(isActive);
+function voids() {
+    console.log("hello");
+}
---
+++ Built-in-type/index.ts
@@ -0,0 +1,12 @@
+let userId:number
+let userName: string
+let isActive: boolean
+userId=101
+userName="TOFAYEL"
+isActive=true
+console.log(isActive);
+
+function voids():void {
+    console.log("hello");
+    
+}
