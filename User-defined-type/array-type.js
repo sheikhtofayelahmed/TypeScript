e1c5373d09462dec2a1f58eb0d4022d8c8436096
tofayel101 <tofayel101@proton.me>
2022-11-20 16:02:47 +0600

    array type

--- User-defined-type/array-type.js
+++ User-defined-type/array-type.js
@@ -1,3 +1,5 @@
 // let userNames: string[]
+// let userNames: (string |number )[]
 var userNames;
-userNames = ['1', '2', '1'];
+userNames = ["1", "2", "1"];
+console.log(userNames);
--- User-defined-type/array-type.ts
+++ User-defined-type/array-type.ts
@@ -1,3 +1,5 @@
 // let userNames: string[]
+// let userNames: (string |number )[]
 let userNames: Array<string>;
-userNames=['1','2','1']
+userNames = ["1", "2", "1"];
+console.log(userNames);
