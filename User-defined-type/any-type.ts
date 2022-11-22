f51e2eb40e27a7d7070659c445d9c93209663b6e
tofayel101 <tofayel101@proton.me>
2022-11-22 10:51:21 +0600

    any type and object

---
+++ User-defined-type/any-type.ts
@@ -0,0 +1,7 @@
+let userName:any;
+userName = "jkj";
+userName = 101;
+userName = [];
+userName = {
+  jack: "hue",
+};
---
+++ User-defined-type/object-type.ts
@@ -0,0 +1,19 @@
+let data:object
+data={
+    name:"tofayel"
+}
+let objectArray:object[]
+objectArray=[
+    {
+        name:"tofayel"
+    }
+]
+
+let users: {
+    name:string,
+    id:number
+}
+users={
+    name:"hello",
+    id:1
+}
